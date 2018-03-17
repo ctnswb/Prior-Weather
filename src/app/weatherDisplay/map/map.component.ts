218bad7b8efa5d6bb98e855560a7da28fb9e4525
ctnswb <xclai.07@gmail.com>
2018-03-16 22:32:42 -0700

    google api does not need a key

--- src/app/weatherDisplay/map/map.component.ts
+++ src/app/weatherDisplay/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import {} from '@types/googlemaps';
 
 @Component({
@@ -9,7 +9,6 @@ import {} from '@types/googlemaps';
 
 export class MapComponent implements OnChanges {
   @Input() coordinates;
-  @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
   marker: google.maps.Marker;
 
